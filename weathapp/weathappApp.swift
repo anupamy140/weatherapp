d87b0ac33f0df211bd38bb423c87be49fe05bdc8
Anupam <Anupam.Yadav@bounteous.com>
2025-11-28 17:11:09 +0530

    Initial Commit

---
+++ weathapp/weathappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weathappApp.swift
+//  weathapp
+//
+//  Created by Anupam Yadav on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct weathappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
